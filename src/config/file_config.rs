803bcb5b6cbd3cde35e6e884f461d31e9a14a162
Leric <leric.zhang@gmail.com>
2021-05-08 15:54:47 +0800

    support config file reload

--- src/config/file_config.rs
+++ src/config/file_config.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use tokio::sync::mpsc;
 use crate::config::{ConfigUpdate, ClientInfo, ServiceInfo};
 use serde::{Serialize, Deserialize};
@@ -22,6 +24,7 @@ pub async fn watch_config(config_file: String, sender: mpsc::Sender<ConfigUpdate
     }
     let _ = sender.send(ConfigUpdate::ConfigReady(true)).await;
 
+    // reload config file on USR2 signal
     let mut usr2 = signal(SignalKind::user_defined2()).expect("Failed to bind on USR2 signal");
     loop {
         usr2.recv().await;
@@ -41,8 +44,35 @@ pub async fn watch_config(config_file: String, sender: mpsc::Sender<ConfigUpdate
 }
 
 
-fn config_diff(_old: &ServiceConfig, _new: &ServiceConfig) -> Vec<ConfigUpdate> {
-    let result = vec![ConfigUpdate::ConfigReady(true)];
+fn config_diff(old: &ServiceConfig, new: &ServiceConfig) -> Vec<ConfigUpdate> {
+    let mut result = Vec::new();
+
+    let mut exist_service: HashMap<String, bool> = HashMap::new();
+    for s in new.services.iter() {
+        exist_service.insert(s.service_id.clone(), true);
+        result.push(ConfigUpdate::ServiceUpdate(s.clone()));
+    }
+    for os in old.services.iter() {
+        if let Some(_) = exist_service.get(&os.service_id) {
+            // pass
+        } else {
+            result.push(ConfigUpdate::ServiceRemove(os.service_id.clone()))
+        }
+    }
+
+    let mut exist_client: HashMap<String, bool> = HashMap::new();
+    for c in new.clients.iter() {
+        exist_client.insert(c.client_id.clone(), true);
+        result.push(ConfigUpdate::ClientUpdate(c.clone()));
+    }
+    for oc in old.clients.iter() {
+        if let Some(_) = exist_client.get(&oc.client_id) {
+            // pass
+        } else {
+            result.push(ConfigUpdate::ClientRemove(oc.client_id.clone()))
+        }
+    }
+
     return result;
 }
 
